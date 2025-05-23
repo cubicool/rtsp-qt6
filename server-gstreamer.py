b8864ae1dd268d14f7664cecee45cb918f13a1f2
Jeremy Moles <cubicool@gmail.com>
2025-05-23 10:14:43 -0400

    Adds the "drop" command to server-gstreamer.py

--- server-gstreamer.py
+++ server-gstreamer.py
@@ -6,6 +6,7 @@ import signal
 import threading
 import sys
 import os
+import shlex
 
 gi.require_version("Gst", "1.0")
 gi.require_version("GstRtspServer", "1.0")
@@ -14,10 +15,110 @@ from gi.repository import Gst, GstRtspServer, GObject, GLib
 
 Gst.init(None)
 
+"""
+class ExampleRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
+	def __init__(self):
+		super().__init__()
+		self.frame_count = 0
+		self.jump_offset = 0
+		self.inject_jump_at = None  # when to apply jump
+		self.jump_applied = False
+
+	def simulate_timestamp_jump(self, offset_ns, trigger_at_frame=30):
+		# Simulate a timestamp jump by offset_ns nanoseconds at frame X.
+		self.jump_offset = offset_ns
+		self.inject_jump_at = trigger_at_frame
+		self.jump_applied = False
+
+	def on_prepared(self, media):
+		element = media.get_element()
+		payloader = element.get_by_name("pay0")
+		pad = payloader.get_static_pad("src")
+
+		# Add probe for timestamp manipulation
+		pad.add_probe(Gst.PadProbeType.BUFFER, self.timestamp_jump_probe)
+
+	def timestamp_jump_probe(self, pad, info):
+		if info.type & Gst.PadProbeType.BUFFER:
+			buf = info.get_buffer()
+
+			if not buf:
+				return Gst.PadProbeReturn.OK
+
+			print(f"[Frame {self.frame_count}] Original PTS: {buf.pts}")
+
+			# Simulate jump
+			if self.inject_jump_at is not None and self.frame_count == self.inject_jump_at and not self.jump_applied:
+				print(f"*** SIMULATING TIME JUMP: +{self.jump_offset / 1e9:.2f} sec ***")
+
+				buf.pts += self.jump_offset
+				self.jump_applied = True
+
+			else:
+				# normal PTS or already jumped
+				pass
+
+			print(f"[Frame {self.frame_count}] New PTS: {buf.pts}")
+
+			self.frame_count += 1
+
+		return Gst.PadProbeReturn.OK
+"""
+
+class DropRTSPMediaFactory(GstRtspServer.RTSPMediaFactory):
+	def __init__(self):
+		super().__init__()
+
+		self.drop_count = 0
+
+	def drop_frames(self, n):
+		print(f"[drop_frames] Request to drop next {n} frames")
+
+		self.drop_count += n
+
+	def do_configure(self, media):
+		media.connect("prepared", self.on_prepared)
+
+	def on_prepared(self, media):
+		element = media.get_element()
+
+		if not element:
+			print("[on_prepared] Failed to get media element")
+
+			return
+
+		payloader = element.get_by_name("pay0")
+
+		if not payloader:
+			print("[on_prepared] Named payloader 'pay0' not found")
+
+			return
+
+		pad = payloader.get_static_pad("src")
+
+		if not pad:
+			print("[on_prepared] Could not get 'src' pad of pay0")
+
+			return
+
+		print("[on_prepared] Attaching drop probe to pay0:src")
+
+		pad.add_probe(Gst.PadProbeType.BUFFER, self.drop_probe)
+
+	def drop_probe(self, pad, info):
+		if self.drop_count > 0:
+			self.drop_count -= 1
+
+			print(f"[drop_probe] Dropping frame... {self.drop_count} left")
+
+			return Gst.PadProbeReturn.DROP
+
+		return Gst.PadProbeReturn.OK
+
 class InteractiveRTSPServer:
 	def __init__(self):
 		self.server = GstRtspServer.RTSPServer()
-		self.factory = GstRtspServer.RTSPMediaFactory()
+		self.factory = DropRTSPMediaFactory()
 		self.factory.set_launch(self._pipeline())
 		self.factory.set_shared(True)
 		self.server.get_mount_points().add_factory("/test", self.factory)
@@ -27,19 +128,20 @@ class InteractiveRTSPServer:
 
 	def start(self):
 		self.server.attach(None)
-		self.factory.connect("media-configure", self.on_media_configure)
+		self.factory.connect("media-configure", self._on_media_configure)
 
-	def on_media_configure(self, factory, media):
-		def on_prepared(media_obj):
+	def _on_media_configure(self, factory, media):
+		def _on_prepared(media_obj):
 			pipeline = media_obj.get_element()
 			src = pipeline.get_by_name("videotestsrc0")
 
 			if src:
 				pad = src.get_static_pad("src")
+
 				self._pad = pad
 				self._media = media_obj
 
-		media.connect("prepared", on_prepared)
+		media.connect("prepared", _on_prepared)
 
 	# def stop(self):
 	# 	print("[server] Stopping stream...")
@@ -64,16 +166,23 @@ class InteractiveRTSPServer:
 			self._pad.remove_probe(self._pad_probe_id)
 			self._pad_probe_id = None
 
+	def drop(self, count):
+		print(f"[server] Request to drop next {count} frames")
+
+		self.factory.drop_count = count
+
 	def _pipeline(self):
 		"""
 		pipeline = (
 			# Creates a "test" source.
 			# https://gstreamer.freedesktop.org/documentation/videotestsrc/?gi-language=c
+			# f"videotestsrc is-live=true pattern={self._patterns()}",
 			f"videotestsrc is-live=true pattern={self._patterns()}",
 
 			# Adds some simulated lag/latency.
 			# https://gstreamer.freedesktop.org/documentation/coreelements/identity.html?gi-language=c
 			# "identity sleep-time=100000",
+			# "identity name=dropper signal-handoffs=true",
 
 			# Encodes into H.264 video.
 			# https://gstreamer.freedesktop.org/documentation/x264/index.html?gi-language=c
@@ -94,7 +203,7 @@ class InteractiveRTSPServer:
 
 			# Force low framerate of 10FPS.
 			# https://gstreamer.freedesktop.org/documentation/additional/design/mediatype-video-raw.html?gi-language=c
-			"video/x-raw,framerate=10/1",
+			"video/x-raw,framerate=15/1",
 
 			# https://gstreamer.freedesktop.org/documentation/pango/timeoverlay.html?gi-language=c
 			# "timeoverlay halign=left valign=bottom font-desc=\"Sans, 24\"",
@@ -144,23 +253,25 @@ class InteractiveRTSPServer:
 		test-pattern      | Pattern designed for video analysis tools
 		"""
 
-		# TODO: What else?
+		# TODO: Make this random!
 		return "ball"
 
-# GLib.timeout_add_seconds(10, stop_stream)
-
 def command_loop(server):
-	print("[input] Type commands: pause / unpause / exit")
+	print("[input] Type commands: pause / unpause / exit / drop <count>")
 
 	while True:
 		try:
-			cmd = sys.stdin.readline().strip().lower()
+			tokens = shlex.split(sys.stdin.readline().strip().lower())
+			cmd, *args = tokens if tokens else ("help", [])
 
 			if cmd == "pause":
 				server.pause()
 
 			elif cmd == "unpause":
 				server.unpause()
+
+			elif cmd == "drop":
+				server.drop(len(args) and int(args[0]) or 10)
 
 			elif cmd == "exit":
 				print("[input] Exiting...")
